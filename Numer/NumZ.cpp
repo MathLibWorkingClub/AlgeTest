b21f232965b5e9142a4f68d5614ccc7eab1b0695
zhuyan0077 <zhuyan0077@126.com>
2018-07-12 20:17:40 +0800

    update of NumZ
    update:
    NumZ::operateor+();
    NumZ::compact();

--- Numer/NumZ.cpp
+++ Numer/NumZ.cpp
@@ -21,9 +21,33 @@ NumZ::NumZ(const NumZ& opd)
 	memcpy(this->bitPat, opd.bitPat, sizeof(uint32_t)*this->bitPatLen);
 }
 
-NumZ NumZ::operator+(const NumZ&)
+NumZ NumZ::operator+(const NumZ& opd)
 {
-	return NumZ();
+	if (this->sign == opd.sign) {
+		NumZ rst = NumZ();
+		rst.bitPatLen = (opd.bitPatLen + this->bitPatLen + abs(opd.bitPatLen - this->bitPatLen)) / 2 + 1;
+		rst.bitPat = new unit32_t[this->bitPatLen];
+		int k = 0;
+		for (size_t i = 0; i < this->bitPatLen; i++) {
+			if (temp->bitPat[i] == opd.bitPat[i]) {
+				rst.bitPat[i] = k;
+				if (opd.bitPat[i] == 1) {
+					k = 1;
+				}
+			}
+	else if (k == 1) {
+				rst.bitPat[i] = 0;
+			}
+	else {
+		rst.bitPat[i] = 1;
+		k = 0;
+	}
+		}
+	}
+}
+rst.compact();
+
+return rst;
 }
 
 NumZ NumZ::operator-(const NumZ&)
@@ -98,4 +122,15 @@ bool NumZ::operator<=(const NumZ&)
 
 void NumZ::compact()
 {
+	size_t k = 0;
+	for (size_t i = this->bitPatLen-1; this->bitPat[ i ] ==0&&i>0; i--)
+	{
+		k++;
+	};
+	this->bitPatLen = this->bitPatLen - k;
+	unit32_t* temp =new unit32_t[this->bitPatLen];
+	memcpy(temp.bitPat, temp.bitPat, sizeof(uint32_t)*this->bitPatLen);
+	delete[]this->bitPat;
+	this->bitPat =temp;
+	
 }
