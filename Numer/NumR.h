85202b4783eb0717d5062fb2d2fbdcb6b909a886
zhangyuace <zhangyuace@foxmail.com>
2018-07-14 20:09:48 +0800

    NumR

--- Numer/NumR.h
+++ Numer/NumR.h
@@ -1,1 +1,42 @@
 #pragma once
+
+#include <cstdlib>
+#include <cstdint>
+#include <string>
+
+#include "Num.h"
+
+class NumR : public Num
+{
+protected:
+	size_t bitPatLen;
+	size_t bitPatPrec;
+	uint32_t* bitPat;
+	Sign sign;
+public:
+	NumR();
+	explicit NumR(size_t len, size_t prec);
+	NumR(size_t len, size_t prec, int_fast32_t* bPat, Sign s);
+	NumR(const NumR&);
+
+	NumR operator+(const NumR&) const;
+	NumR operator-(const NumR&) const;
+	NumR operator*(const NumR&) const;
+	NumR operator/(const NumR&) const;
+	NumR operator%(const NumR&) const;
+
+	void operator+=(const NumR&);
+	void operator-=(const NumR&);
+	void operator*=(const NumR&);
+	void operator/=(const NumR&);
+	void operator%=(const NumR&);
+
+	bool operator==(const NumR&) const;
+	bool operator!=(const NumR&) const;
+	bool operator>(const NumR&) const;
+	bool operator<(const NumR&) const;
+	bool operator>=(const NumR&) const;
+	bool operator<=(const NumR&) const;
+
+	void compact(); // Shorten the bitPat by remove leading-zeros
+};
